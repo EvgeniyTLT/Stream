1645b93ffdf43546c0fbf71749430869524ab588
EvgeniyTLT <Toy444@ya.ru>
2022-06-16 23:34:17 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         new Thread(() -> System.out.println(1)).start();
+        Director director = new Director();
+        director.force(new Worker() {
+            @Override
+            public void work() {
+                System.out.println("working ....");
+            }
+        });
     }
 }
