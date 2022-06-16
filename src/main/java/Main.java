2b75ad44efd1ba412f44e4882bd7af39b602c57a
EvgeniyTLT <Toy444@ya.ru>
2022-06-17 00:06:26 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        new Thread(() -> System.out.println(1)).start();
-        Director director = new Director();
-        String result = director.force((int n)->{
-            for (int i = 0; i < n; i++) {
-                System.out.println("Working...");
+//        new Thread(() -> System.out.println(1)).start();
+//        Director director = new Director();
+//        String result = director.force((int n)->{
+//            for (int i = 0; i < n; i++) {
+//                System.out.println("Working...");
+//            }
+//            return "Success";
+//        }, 5);
+//        System.out.println(result);
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            numbers.add((int) (Math.random() * 1000));
+        }
+
+        for (int i:filter(numbers, 7)
+             ) {
+            System.out.println(i);
+        }
+
+
+
+
+
+    }
+
+    public static List<Integer> filter(List<Integer> list, int a) {
+        List<Integer> result = new ArrayList<>();
+        for (int i : list) {
+            if (new Predicate() {
+                @Override
+                public boolean test(int a) {
+                    if (i % a == 0) {
+                        return true;
+                    } else return false;
+                }
+            }.test(5) == true) {
+                result.add(i);
             }
-            return "Success";
-        }, 5);
-        System.out.println(result);
+        }
+        return result;
     }
 }
