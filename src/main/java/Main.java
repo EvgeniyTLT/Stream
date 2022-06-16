97d694ec90e7771197413fb502bf8c1757165701
EvgeniyTLT <Toy444@ya.ru>
2022-06-16 23:44:03 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         new Thread(() -> System.out.println(1)).start();
         Director director = new Director();
-        director.force(() -> System.out.println("working ...."));
+        String result = director.force((int n)->{
+            for (int i = 0; i < n; i++) {
+                System.out.println("Working...");
+            }
+            return "Success";
+        }, 5);
+        System.out.println(result);
     }
 }
