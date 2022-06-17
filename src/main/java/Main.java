fa3d48b2d263331763669bc6162d028fabcba789
EvgeniyTLT <Toy444@ya.ru>
2022-06-18 00:24:31 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,21 +6,38 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+////
+//        List<Integer> numbers = new ArrayList<>();
+//        for (int i = 0; i < 100; i++) {
+//            numbers.add((int) (Math.random() * 1000));
+//        }
 //
+//        List<String> list = numbers.stream()
+//                .filter(integer -> integer % 2 == 0)
+//                .map((Function<Integer, String>) integer -> "Number: " + integer)
+//                .filter((string) -> string.endsWith("0"))
+//                .map((string) -> string + "!")
+//                .collect(Collectors.toList());
+//
+//
+//        for (String i : list
+//        ) {
+//            System.out.println(i);
+//        }
+
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            numbers.add((int) (Math.random() * 1000));
+            numbers.add((int) (Math.random() * 100 + 100));
         }
 
-        List<String> list = numbers.stream()
-                .filter(integer -> integer % 2 == 0)
-                .map((Function<Integer, String>) integer -> "Number: " + integer)
-                .filter((string) -> string.endsWith("0"))
-                .map((string) -> string + "!")
+
+        List<String> list2 = numbers.stream()
+                .filter(integer -> integer % 2 == 0 && integer % 5 == 0)
+                .map(integer -> Math.sqrt(integer))
+                .map(sqrt -> "Корень: " + sqrt)
                 .collect(Collectors.toList());
 
-
-        for (String i : list
+        for (String i : list2
         ) {
             System.out.println(i);
         }
