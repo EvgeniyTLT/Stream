fad871f63b0d1188f95540cb43721e4bf0ad52d3
EvgeniyTLT <Toy444@ya.ru>
2022-06-17 23:57:00 +0300

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-//        new Thread(() -> System.out.println(1)).start();
-//        Director director = new Director();
-//        String result = director.force((int n)->{
-//            for (int i = 0; i < n; i++) {
-//                System.out.println("Working...");
-//            }
-//            return "Success";
-//        }, 5);
-//        System.out.println(result);
+//
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             numbers.add((int) (Math.random() * 1000));
         }
 
-        List<Integer> filtered = filter(numbers, (n) -> n % 2 == 0);
-        List<String> mapped = map(filtered);
-        for (String map : mapped
+        List<Integer> filtered = numbers.stream()
+                .filter(integer -> integer % 2 == 0)
+                .map((Function<Integer, String>) integer -> "Number: " + integer)
+
+
+        for (String i : mapped
         ) {
-            System.out.println(map);
+            System.out.println(i);
         }
 
 
