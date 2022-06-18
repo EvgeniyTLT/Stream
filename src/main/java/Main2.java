31b755c5d933b45416e35ef3d727d5eb349e7ce9
EvgeniyTLT <Toy444@ya.ru>
2022-06-18 12:54:43 +0300

    add

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main2 {
     public static void main(String[] args) {
@@ -14,10 +16,27 @@ public class Main2 {
         users.add(new User("Helen", 11));
 
 
+//        long count = users.stream()
+//                .filter((user -> user.getAge()>30))
+//                .count(); // считает количество оставшихся элементов
 
+//        boolean count = users.stream()
+//                .allMatch(user -> user.getAge() >18); // проверяте на соответствие
 
-        long count = users.stream()
-                .filter((user -> user.getAge()>30))
-                .count();
+//        boolean count = users.stream()
+//                .anyMatch(user -> user.getAge() >18); // если есть хоть одно true, возвращает true
+
+//        boolean count = users.stream()
+//                .noneMatch(user -> user.getAge() <= 18); // есть все false, вернет true
+
+        List<User> sorted = users.stream()
+                .sorted((o1, o2) -> Integer.compare(o2.getAge(), o1.getAge()))
+                .limit(3)
+                .collect(Collectors.toList());
+
+        for (User user: sorted
+             ) {
+            System.out.println(user.getName());
+        }
     }
 }
